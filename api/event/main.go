526d0d2024bcffea488c5f27037bfbe13d99584b
totalbug92 <jakob.fonstad92@gmail.com>
2018-11-19 23:17:24 +0100

    Update api/event/main.go
    working on api

--- api/event/main.go
+++ api/event/main.go
@@ -1,6 +1,7 @@
 package event
 
 import (
+	"github.com/gorilla/mux"
 	"github.com/kongebra/cpts/api/ticket"
 	"gopkg.in/mgo.v2/bson"
 )
@@ -16,6 +17,16 @@ type Event struct {
 	Description string `json:"description"`
 	Date TimeInterval `json:"date"`
 	Participants []bson.ObjectId `json:"participants"`
+}
+
+func main(){
+	r := mux.NewRouter()
+
+	r.HandleFunc("/api/events/", getEvents).Methods("GET")
+	r.HandleFunc("/api/events/{id}", getEvent).Methods("GET")
+	r.HandleFunc("/api/events", createEvent).Methods("POST")
+	r.HandleFunc("/api/events/{id}", updateEvents).Methods("PUT")
+	r.HandleFunc("/api/events/{id}", deleteEvents).Methods("DELETE")
 }
 
 func (e *Event) CreateTicket() ticket.Ticket {
